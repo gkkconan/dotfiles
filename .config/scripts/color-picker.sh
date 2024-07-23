084fb57f854eeecd4c2e96856d6e6bc7f25fc2e1
gkkconan <conan.gkk@gmail.com>
2024-07-23 16:22:43 +0200

    fix(color-picker): update and improve color picker script

--- .config/scripts/color-picker.sh
+++ .config/scripts/color-picker.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+#   ____ ___  _     ___  ____      ____ ___ ____ _  _______ ____  
+#  / ___/ _ \| |   / _ \|  _ \    |  _ \_ _/ ___| |/ / ____|  _ \ 
+# | |  | | | | |  | | | | |_) |   | |_) | | |   | ' /|  _| | |_) |
+# | |__| |_| | |__| |_| |  _ <    |  __/| | |___| . \| |___|  _ < 
+#  \____\___/|_____\___/|_| \_\   |_|  |___\____|_|\_\_____|_| \_\
+#
+# Author: gkkconan
+# Contact: conan.gkk@gmail.com
 
-# ###################### #
-#      COLOR PICKER      #
-# ###################### #
+
 
 color=$(hyprpicker)
 
 if [ -n "$color" ]; then
-    dunstify -a System -u normal "Selected Color: $color" --action="default,Copy"
-    wl-copy $color
-    dunstify -a System -u normal "Color: $color copied" -t 1500
+    notify=$(dunstify -a System -u normal "Selected Color: $color" --action="default, Copy" -h string:x-dunst-stack-tag:color-picker)
+    [ "$notify" = "default" ] && {
+        wl-copy $color
+        dunstify -a System -u normal "Selected Color: $color copied" -t 1500 -h string:x-dunst-stack-tag:color-picker
+    }
 fi

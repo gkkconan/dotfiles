c852029eb3a52713ec0bd4ef0bb007388656241d
gkkconan <conan.gkk@gmail.com>
2024-07-25 12:01:48 +0200

    feat: add fish shell configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path ~
+# set fish_greeting ""
+
+
+###----- FUNCTIONS -----###
+function obsidian
+	nohup /var/lib/flatpak/app/md.obsidian.Obsidian/aarch64/stable/7b879abbc0a5e2686b07b15c8fe3fde870a963fe7a4194a7d5286c14b0670256/export/bin/md.obsidian.Obsidian > /dev/null 2>&1 &
+	disown
+end
+
+function fish_greeting
+	echo 'Welcome'(set_color DF1722) $USER ':)'
+end

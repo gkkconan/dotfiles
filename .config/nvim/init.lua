596ad8e829a8ad59f17086664ab6b6927635fca9
gkkconan <conan.gkk@gmail.com>
2024-07-27 20:20:37 +0200

    feat(nvim): update configs and install plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,134 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.cmd("set number")
+vim.cmd("set nuw=3")
+vim.cmd("set softtabstop=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.opt.fillchars = { eob = " " }
+
+-- lazy set up
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+local plugins = {
+	-- miasma (theme)
+	{
+		"xero/miasma.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme miasma")
+		end,
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("nvim-tree").setup {}
+		end,
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ 'm4xshen/autoclose.nvim' },
+	{ 'romgrk/barbar.nvim' },
+	{ 'xiyaowong/transparent.nvim' },
+	{ 'EdenEast/nightfox.nvim' },
+	{ 'nvim-tree/nvim-tree.lua' }
+}
+
+
+-- setup
+require("lazy").setup(plugins, opts)
+require("autoclose").setup()
+require("transparent").setup()
+
+require("barbar").setup{
+	version = '^1.0.0'
+}
+
+require("lualine").setup{
+	options = {
+		theme = 'auto'
+	}
+}
+
+require("catppuccin").setup({transparent_background = false})
+
+vim.cmd.colorscheme "catppuccin"
+
+
+
+
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+
+-- Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+
+
+
+
+-- nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+require("nvim-tree").setup()
+
+vim.keymap.set('n', '<C-b>', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+
+
+-----------------------------------------------------------------------
+
+
+-- Sposta la riga verso il basso
+vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-j>', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
+
+-- Sposta la riga verso l'alto
+vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })

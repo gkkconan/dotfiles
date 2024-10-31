8072467efc053aea1b2e5f777cc4561dfe6a6d01
gkkconan <conan.gkk@gmail.com>
2024-10-31 21:46:48 +0100

    feat: add change theme script

---
+++ .config/scripts/change-theme.sh
@@ -0,0 +1,29 @@
+
+currentTheme=$(grep "^theme=" ~/.environment | cut -d '=' -f2)
+
+if [ -z "$currentTheme" ]; then
+	echo "theme=light" | tee -a ~/.environment > /dev/null
+else
+	if [ "$currentTheme" = "light" ]; then
+		currentTheme="dark"
+	else
+		currentTheme="light"
+	fi
+	sed -i "s/^theme=.*/theme=$currentTheme/" ~/.environment
+	export theme=$currentTheme
+fi
+
+
+pid=$(pgrep swww)
+
+if [ -n "$pid" ]; then killall swww; fi
+swww-daemon
+
+if [[ $currentTheme = "light" ]]; then
+	swww img --transition-type center --transition-pos 20,20 ~/.config/hypr/wallpapers/escher-light-wallpaper.png
+	echo "@define-color theme rgb(0, 0, 0); @define-color coral-pink #FF928B;" > ~/.config/hypr/palette.css
+elif [[ $currentTheme = "dark" ]]; then
+	swww img --transition-type center --transition-pos 20,20 ~/.config/hypr/wallpapers/black-pantone-color-wallpaper.jpg
+	echo "@define-color theme rgb(255, 255, 255); @define-color coral-pink #FF928B;" > ~/.config/hypr/palette.css
+fi
+

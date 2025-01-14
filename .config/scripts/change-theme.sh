068908a60174d72df842366cf94e8e4b0bc3395a
gkkconan <conan.gkk@gmail.com>
2025-01-14 02:24:35 +0100

    chore(scripts/change-theme): implement windows border color switching

--- .config/scripts/change-theme.sh
+++ .config/scripts/change-theme.sh
@@ -22,9 +22,11 @@ swww-daemon
 
 if [[ $currentTheme = "light" ]]; then
 	swww img --transition-type center --transition-pos 20,20 ~/.config/hypr/wallpapers/escher-light-wallpaper.png
+	sed -i 's/col\.active_border = rgb(ffffff)/col\.active_border = rgb(000000)/g' ~/.config/hypr/hyprland.conf
 	echo "@define-color theme rgb(0, 0, 0); @define-color coral-pink #FF928B;" > ~/.config/hypr/palette.css
 elif [[ $currentTheme = "dark" ]]; then
 	swww img --transition-type center --transition-pos 20,20 ~/.config/hypr/wallpapers/black-pantone-color-wallpaper.jpg
+	sed -i 's/col\.active_border = rgb(000000)/col\.active_border = rgb(ffffff)/g' ~/.config/hypr/hyprland.conf
 	echo "@define-color theme rgb(255, 255, 255); @define-color coral-pink #FF928B;" > ~/.config/hypr/palette.css
 fi
 
